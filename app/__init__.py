b0dd76b10b61ad2c3f92a9e1cf13aa90331fa19c
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-03 14:51:34 +0300

    I have created a bootstrap instance

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,16 @@ from flask import Flask
 from flask_bootstrap import flask_bootstrap
 from config import config_options
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
-# def form_app(config_name):
+def form_app(config_name):
 
 
-#     app =Flask(__name__):
+    app =Flask(__name__):
 
-#     app.config.from_object(config_options[config_name])
+    app.config.from_object(config_options[config_name])
 
-#     bootstrap.init_app(app)
+    bootstrap.init_app(app)
 
-#     return app
+    return app
 
