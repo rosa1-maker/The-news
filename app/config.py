5ae774024d8f776ebe72cba7ed313976c49cb2e8
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-03 13:55:33 +0300

    imported the os module so as to interact with the operating system

---
+++ app/config.py
@@ -0,0 +1,22 @@
+import os
+
+# class Config:
+
+#     NEW_API_BASE_URL = '{}?api_key={}''
+#     MOVIE_API_KEY = os.environ.get('NEW_API_KEY')
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+# class ProdConfig(Config):
+#     pass
+
+
+
+# class DevConfig(Config):
+#     DEBUG = True
+
+
+# config_options ={
+#     'development':DevConfig,
+#     'production' :ProdConfig
+# }
