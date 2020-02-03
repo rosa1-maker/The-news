ced0b85ebf7c260931158693b2cd8bcf5034108c
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-03 13:58:41 +0300

    crete a dictionary class config to acess configuration option classess

--- app/config.py
+++ app/config.py
@@ -1,22 +1,22 @@
 import os
 
-# class Config:
+class Config:
 
-#     NEW_API_BASE_URL = '{}?api_key={}''
-#     MOVIE_API_KEY = os.environ.get('NEW_API_KEY')
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
-# class ProdConfig(Config):
-#     pass
+    NEW_API_BASE_URL = '{}?api_key={}''
+    MOVIE_API_KEY = os.environ.get('NEW_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
+class ProdConfig(Config):
+    pass
 
-# class DevConfig(Config):
-#     DEBUG = True
 
 
-# config_options ={
-#     'development':DevConfig,
-#     'production' :ProdConfig
-# }
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options ={
+    'development':DevConfig,
+    'production' :ProdConfig
+}
