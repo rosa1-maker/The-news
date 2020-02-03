cd8efeec354116b642015aed2dc310081548e1a1
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-03 14:31:34 +0300

    imported the config options

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_bootstrap import flask_bootstrap
+from config import config_options
+
+# bootstrap = Bootstrap()
+
+# def form_app(config_name):
+
+
+#     app =Flask(__name__):
+
+#     app.config.from_object(config_options[config_name])
+
+#     bootstrap.init_app(app)
+
+#     return app
+
