6b06edd249c4cdc4ed4cd413de660bed9be0a2d4
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-04 05:13:38 +0300

    created a function to configure the variable value to the configuration objects

--- request.py
+++ request.py
@@ -5,3 +5,8 @@ from models import News
 api_key = None
 base_url = None
 
+def configure_request(app):
+    global api_key,base_url1
+    api_key = app.config['MOVIE_API_KEY']
+    basse_url = app.config['MOVIE_API_BASE_URL']
+    
