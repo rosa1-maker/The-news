4af6637254d53db270d018134deb149c81fae360
rosa1-maker <teresawanjiku2000@gmail.com>
2020-02-04 05:19:03 +0300

    imported configure_request init.py

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,12 @@ def form_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
 
+    from.request import configure_request
+    configure_request(app)
+
+    
     return app
+
 
